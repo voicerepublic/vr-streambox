1e237a12f1762afed17a7021dfaaad4c8182ec8f
phil <phil@branch14.org>
2016-03-30 15:41:13 +0200

    endless loop instead of launchtool

--- launcher.sh
+++ launcher.sh
@@ -6,4 +6,10 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Initial launch..."
 
-/usr/bin/launchtool -t streambox -u root -g root -Lnvc $DIR/update_and_start.sh
+while :
+do
+  $DIR/update_and_start.sh
+	sleep 5
+done
+
+# /usr/bin/launchtool -t streambox -u root -g root -Lnvc $DIR/update_and_start.sh
