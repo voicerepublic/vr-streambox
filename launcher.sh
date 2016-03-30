b3a342ae5101296e79baa1d5a86ec141fd0b050f
phil <phil@branch14.org>
2016-03-30 16:42:54 +0200

    fix permission of keys

--- launcher.sh
+++ launcher.sh
@@ -9,6 +9,7 @@ do
     echo "Copy keys..."
     mkdir -p /root/.ssh
     cp $DIR/id_rsa* /root/.ssh
+    chmod 600 /root/.ssh/id_rsa*
 
     echo "Updating..."
     (cd $DIR && git pull origin master)
