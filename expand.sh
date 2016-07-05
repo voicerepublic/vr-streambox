16b3f3006f175800f45522e8641784175bcf5663
Lino von Burg <linovonburg@gmail.com>
2016-07-05 14:03:56 +0200

    moved auto-expand in its own script

---
+++ expand.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# This file's responsibility is to expand the file system on the first run.
+#
+# This script will be executed once on each box, if the file in $SHOULD_AUTO_EXPAND
+# exists. After that, it will not run
+#
+
+SHOULD_AUTO_EXPAND=/boot/auto_expand
+NEED_AUTO_EXPAND=$HOME/expanded
+
+
+if [ ! -e $NEED_AUTO_EXPAND ]; then
+  if [ -e $SHOULD_AUTO_EXPAND ]; then
+
+    message "Checking if Filesystem needs expanding..."
+
+    SIZE=$(df --output=size,target | grep /$ | sed -e /Size/d | sed 's: /$::g')
+
+    if [ "$SIZE" -lt "2000000" ]; then
+      message "Expanding Filesystem..."
+      sudo raspi-config --expand-rootfs
+      touch $NEED_AUTO_EXPAND
+      message "Rebooting in 10 seconds..."
+      sleep 10
+      sudo reboot
+    else
+      message "Filesystem already expanded, continuing."
+      touch $NEED_AUTO_EXPAND
+    fi
+  else
+    message "$SHOULD_AUTO_EXPAND not present, will not automatically expand filesystem!"
+  fi
+fi
