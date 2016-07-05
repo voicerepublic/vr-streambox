1e1c904e3a7a16278df7166260fcacbf411110f5
Lino von Burg <linovonburg@gmail.com>
2016-07-05 15:24:30 +0200

    reduced wait time before reboot

--- expand.sh
+++ expand.sh
@@ -26,8 +26,8 @@ if [ ! -e $NEED_AUTO_EXPAND ]; then
       message "Expanding Filesystem..."
       sudo raspi-config --expand-rootfs
       touch $NEED_AUTO_EXPAND
-      message "Rebooting in 10 seconds..."
-      sleep 10
+      message "Rebooting..."
+      sleep 1
       sudo reboot
     else
       message "Filesystem already bigger than 2GB, assuming expansion already took place and continuing."
