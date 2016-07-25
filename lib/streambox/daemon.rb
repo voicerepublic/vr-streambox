e2d75862094b08769f480a9c2aae12dc263c5a19
phil <phil@branch14.org>
2016-07-25 11:10:03 +0200

    add playing pairing code

--- lib/streambox/daemon.rb
+++ lib/streambox/daemon.rb
@@ -90,11 +90,23 @@ module Streambox
       puts
       system('figlet -t "  %s"' % code)
       puts
-      puts 'Go to the following url to claim this device.'
+      puts 'Visit the following URL to claim this device.'
       puts
       puts '  https://voicerepublic.com/devices/%s' % code
       puts
       puts '*' * 60
+    end
+
+    def play_pairing_code
+      Thread.new do
+        url = @config.endpoint.sub('/api/devices', "/tts/#{@config.pairing_code}")
+        system("curl -L #{url} > code.ogg")
+        system("amixer set PCM 100%")
+        while @config.status == 'pairing'
+          system('ogg123 -q code.ogg')
+          sleep 1.5
+        end
+      end
     end
 
     def start_heartbeat
@@ -163,7 +175,10 @@ module Streambox
     def run
       knock
       register
-      display_pairing_instructions if @config.state == 'pairing'
+      if @config.state == 'pairing'
+        display_pairing_instructions
+        play_pairing_code
+      end
       start_heartbeat
       start_reporting
       start_recording
