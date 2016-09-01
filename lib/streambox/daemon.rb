d81ff9623dc622b844cda6ef66d601fdef4758da
phil <phil@branch14.org>
2016-09-01 17:51:52 +0200

    fix

--- lib/streambox/daemon.rb
+++ lib/streambox/daemon.rb
@@ -216,7 +216,7 @@ module Streambox
 
     def heartbeat
       response = put(device_url)
-      @network = response.status != 200
+      @network = response.status == 200
       if @prev_network != @network
         logger.warn "[NETWORK] #{@network ? 'UP' : 'DOWN'}"
         @prev_network = @network
