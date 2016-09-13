44a5f1dff913e3a78938d910ca63448807ffbce2
phil <phil@branch14.org>
2016-09-13 14:21:59 +0200

    write record messages to fifo

--- record.sh
+++ record.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sox -q -t alsa $DEVICE recordings/recording_`date +%s`_%4n.ogg \
-    silence 1 1.0 5% 1 10.0 5% : newfile : restart
+    silence 1 1.0 5% 1 10.0 5% : newfile : restart 2>&1 > record.sh.log
