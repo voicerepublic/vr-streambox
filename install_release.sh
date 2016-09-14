e0083bb6529876b2b8e8ab67ff7d13c66e236da9
phil <phil@branch14.org>
2016-09-14 10:50:43 +0200

    debug install release

--- install_release.sh
+++ install_release.sh
@@ -6,15 +6,20 @@ BASE="https://gitlab.com/voicerepublic/streambox/repository/archive.tar.gz"
 
 SOURCE="$BASE?ref=v$VERSION&private_token=$TOKEN"
 
+echo $SOURCE
+
 curl -s -L "$SOURCE" > ../archive.tar.gz
+
+ls -la ../archive.tar.gz
+file ../archive.tar.gz
 
 (cd ..; tar xfz archive.tar.gz)
 
-RECENT=`ls -dArt ../streambox-master-* | tail -n 1`
+RECENT=`ls -dArt ../streambox-v* | tail -n 1`
 
 sync
 
-ln -sf `basename $RECENT` ../streambox
+echo ln -sf `basename $RECENT` ../streambox
 
 sync
 
