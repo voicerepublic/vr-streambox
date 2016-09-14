d2a0335529c9ea8ff9740185982c57b6e9c23e19
phil <phil@branch14.org>
2016-09-14 14:55:16 +0200

    inc buffer

--- record.sh
+++ record.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ../recordings
 
-sox -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
+sox --buffer 100000 -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
     silence 1 1.0 5% 1 10.0 5% : newfile : restart >record.sh.log 2>&1
