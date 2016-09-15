4b86191b2135d340273c3402e1177f5b5573a7c4
phil <phil@branch14.org>
2016-09-15 09:16:05 +0200

    make sure there is no old sync.log in place

--- sync.sh
+++ sync.sh
@@ -11,6 +11,8 @@
 (
     cd ../recordings
 
+    rm -f sync.log
+
     aws s3 sync . s3://$BUCKET/$IDENTIFIER --region $REGION \
         >../streambox/sync.log 2>&1 && \
         (ls -1tp | tail -n +2 | xargs -I {} rm -- {})
