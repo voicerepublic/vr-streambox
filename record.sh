fff25a2f3e8861668a015d07a952ddabe053c18c
phil <phil@branch14.org>
2016-09-20 14:25:32 +0200

    make recording silent again

--- record.sh
+++ record.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ../recordings
 
-#sox --buffer 200000 -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
-#    silence 1 1.0 5% 1 10.0 5% : newfile : restart >record.log 2>&1
+sox --buffer 200000 -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
+    silence 1 1.0 5% 1 10.0 5% : newfile : restart >record.log 2>&1
 
-sox -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
-    silence 1 1.0 5% 1 10.0 5% : newfile : restart
+#sox -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
+#    silence 1 1.0 5% 1 10.0 5% : newfile : restart
