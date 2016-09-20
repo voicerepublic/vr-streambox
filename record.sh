cfa87e9f2be2e8b3a177bdf4338fcc21063d721c
phil <phil@branch14.org>
2016-09-20 17:12:55 +0200

    log if sox comes to an end, then wait 3 seconds

--- record.sh
+++ record.sh
@@ -5,5 +5,9 @@ mkdir -p ../recordings
 sox -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
     silence 1 1.0 5% 1 10.0 5% : newfile : restart >record.log 2>&1
 
+echo 'Recording terminated. Is an audio device plugged in?' >record.log
+
+sleep 3
+
 #sox -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
 #    silence 1 1.0 5% 1 10.0 5% : newfile : restart
