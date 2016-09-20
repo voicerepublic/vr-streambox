7c97ce32f91e0bf11356ee90a6e644de203f9509
phil <phil@branch14.org>
2016-09-20 14:18:11 +0200

    improve sync script

--- sync.sh
+++ sync.sh
@@ -14,6 +14,15 @@
     rm -f sync.log
 
     aws s3 sync . s3://$BUCKET/$IDENTIFIER --region $REGION \
-        >../streambox/sync.log 2>&1 && \
-        (ls -1tp | tail -n +2 | xargs -I {} rm -- {})
+        >>../streambox/sync.log 2>&1
+
+    if [ $? -eq 0 ]; then
+        # find the file which is currently being written to and delete all others
+        ACTIVE_PATH=`lsof -c sox | grep recordings | tr -s ' ' | cut -d ' ' -f 9`
+        ACTIVE_FILE=`basename $ACTIVE_PATH`
+        ls -1 | grep -v $ACTIVE_FILE | xargs -I {} rm -- {}
+        echo "Cleanup after sync complete." >>../streambox/sync.log
+    else
+        echo "Sync failed, skipping cleanup." >>../streambox/sync.log
+    fi
 )
