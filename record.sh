e94b8463b0affbb7fb0ea99f3993ba3ad45c70a1
phil <phil@branch14.org>
2016-09-20 15:35:56 +0200

    remove buffer param

--- record.sh
+++ record.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ../recordings
 
-sox --buffer 200000 -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
+sox -q -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
     silence 1 1.0 5% 1 10.0 5% : newfile : restart >record.log 2>&1
 
 #sox -t alsa $DEVICE ../recordings/recording_`date +%s`_%4n.ogg \
