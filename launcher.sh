0620ddbb0befca91e4e0bd9472808b5b5c135185
phil <phil@branch14.org>
2016-09-28 15:25:16 +0200

    also link restarting message

--- launcher.sh
+++ launcher.sh
@@ -104,7 +104,7 @@ do
     sleep 5
 
     # slack
-    TEXT="$NAME $SERIAL with v$VERSION on $PRIVATE_IP restarting..."
+    TEXT="$NAME <$URL|$SERIAL> with v$VERSION on $PRIVATE_IP restarting..."
     JSON='{"channel":"#streamboxx","text":"'$TEXT'","icon_emoji":":satellite:","username":"streamboxx"}'
     curl -X POST -H 'Content-type: application/json' --data "$JSON" \
          https://hooks.slack.com/services/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
