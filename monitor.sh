f40326ce38bee73d4eb64e2d36acbda0afbd86ff
phil <phil@branch14.org>
2016-10-07 11:12:21 +0200

    formatting ii

--- monitor.sh
+++ monitor.sh
@@ -4,19 +4,17 @@ cd ~pi/streambox
 
 mkdir -p ../recordings
 
-/usr/bin/watch -n 3 \
-  "echo '--------------------------------------------------------------------------------'
-   uptime; \
+/usr/bin/watch -t -n 3 \
+  "uptime; \
    test -e /boot/dev_box && echo 'DEV BOX'; \
-   cat VERSION; \
-   cat ../subtype; \
-   echo; \
+   echo -n 'version         : '; cat VERSION; \
+   echo -n 'subtype         : '; cat ../subtype; echo; \
    grep Serial /proc/cpuinfo; \
-   echo -n 'eth0       '; cat /sys/class/net/eth0/address; \
-   echo -n 'wlan0      '; cat /sys/class/net/wlan0/address; \
-   echo -n 'private ip '; hostname -I | cut -d ' ' -f 1; \
-   vcgencmd measure_temp; \
+   echo -n 'eth0            : '; cat /sys/class/net/eth0/address; \
+   echo -n 'wlan0           : '; cat /sys/class/net/wlan0/address; \
+   echo -n 'private ip      : '; hostname -I | cut -d ' ' -f 1; \
    cat /proc/meminfo | head -3; \
+   vcgencmd measure_temp; \
    ps aux | grep sox | grep -v grep; \
    ps aux | grep darkice | grep -v grep; \
    df -h; \
