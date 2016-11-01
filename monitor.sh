6c47e82c46e3e4731520876cb01fd9fb83408af3
phil <phil@branch14.org>
2016-11-01 10:33:48 +0100

    add networking to monitor script

--- monitor.sh
+++ monitor.sh
@@ -12,25 +12,31 @@
        echo -n 'version         : '; cat VERSION; \
        echo -n 'subtype         : '; cat ../subtype; echo; \
        grep Serial /proc/cpuinfo; \
-       echo -n 'eth0            : '; cat /sys/class/net/eth0/address; \
-       echo -n 'wlan0           : '; cat /sys/class/net/wlan0/address; \
-       echo -n 'private ip      : '; hostname -I | cut -d ' ' -f 1; \
        cat /proc/meminfo | head -3; \
        vcgencmd measure_temp | sed 's/=/            : /'; \
        echo -n 'record pid      : '; cat ../record.sh.pid; echo; \
        test -e ../darkice.pid && (echo -n 'darkice pid     : '; cat ../darkice.pid; echo); \
 
-       ps aux | grep launcher | grep -v grep; \
-       ps aux | grep ruby | grep -v grep; \
+       ps aux | grep launcher  | grep -v grep; \
+       ps aux | grep ruby      | grep -v grep; \
        ps aux | grep record.sh | grep -v grep; \
-       ps aux | grep sox | grep -v grep; \
-       ps aux | grep darkice | grep -v grep; \
-       ps aux | grep sync.sh | grep -v grep; \
-       ps aux | grep aws | grep -v grep; \
+       ps aux | grep sox       | grep -v grep; \
+       ps aux | grep darkice   | grep -v grep; \
+       ps aux | grep sync.sh   | grep -v grep; \
+       ps aux | grep aws       | grep -v grep; \
 
        df -h; \
 
-       ls -lA ../recordings"
+       ls -lA ../recordings; \
+
+       echo -n 'eth0 mac        : '; cat /sys/class/net/eth0/address; \
+       echo -n 'wlan0 mac       : '; cat /sys/class/net/wlan0/address; \
+       echo -n 'eth0 state      : '; cat /sys/class/net/eth0/operstate; \
+       echo -n 'wlan0 state     : '; cat /sys/class/net/wlan0/operstate; \
+       echo -n 'private ip      : '; hostname -I | cut -d ' ' -f 1; \
+       ifconfig eth0; \
+       ifconfig wlan0; \
+       iwconfig wlan0"
 
 #else
 
