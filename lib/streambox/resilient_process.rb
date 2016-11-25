4b9e951f74eb4abf34786cc87610281457631901
phil <phil@branch14.org>
2016-11-25 15:45:01 +0100

    improve debugging messages

--- lib/streambox/resilient_process.rb
+++ lib/streambox/resilient_process.rb
@@ -18,7 +18,7 @@ module Streambox
     attr_accessor :watchdog
 
     def initialize(cmd, name, interval, delay, logger)
-      logger.debug "[RESILIENT] Init #{name}"
+      logger.debug "[RESILIENT] init: #{name}"
 
       self.cmd = cmd
       self.name = name
@@ -28,21 +28,21 @@ module Streambox
 
       @pidfile = "../#{name}.pid"
       @pidilfe = File.expand_path(@pidfile, Dir.pwd)
-      logger.debug "[RESILIENT] File: #{@pidilfe}"
+      logger.debug "[RESILIENT] init: File: #{@pidilfe}"
 
       @pid = File.read(@pidfile).to_i if File.exist?(@pidfile)
       @pid = nil unless exists?
 
       if @pid
-        logger.debug "[RESILIENT] Detected #{@pid} for #{name}. Resume running..."
+        logger.debug "[RESILIENT] init: Detected #{@pid} for #{name}. Resume running..."
         start!
       else
-        logger.debug "[RESILIENT] No running process for #{name}, waiting for start."
+        logger.debug "[RESILIENT] init: No running process for #{name}, waiting for start."
       end
     end
 
     def start!
-      logger.debug "[RESILIENT] ================================================== START!, watchdog: #{watchdog}"
+      logger.debug "[RESILIENT] start!: ================================================== START!, watchdog: #{watchdog}"
       # guard against multiple runs in paralell
       return if watchdog and watchdog.alive?
 
@@ -51,23 +51,24 @@ module Streambox
       @thread_counter ||= 0
 
       if @pid
-        logger.debug "[RESILIENT] Found #{@pid} for #{name}."
+        logger.debug "[RESILIENT] start!: Found #{@pid} for #{name}."
       else
-        logger.debug "[RESILIENT] Found none for #{name}."
+        logger.debug "[RESILIENT] start!: Found none for #{name}."
       end
 
       self.watchdog = Thread.new do
         @thread_counter += 1
-        logger.debug "[RESILIENT] Start watching #{name}."
-        start_new unless exists?
+        logger.debug "[RESILIENT] start!: Start watching #{name}."
+        #start_new unless exists?
         @cycle_counter = 0
         while true
           @cycle_counter += 1
-          logger.debug "[RESILIENT] ================================================== start: %s, thread: %s, cycle: %s" %
+          logger.debug "[RESILIENT] start!: ================================================== start: %s, thread: %s, cycle: %s" %
                        [@start_counter, @thread_counter, @cycle_counter]
-          start_new(delay) unless exists?
+          start_new unless exists?
           #logger.debug "Waiting for pid #{@pid} for #{name}"
           wait
+          sleep delay
         end
         #logger.debug "[RESILIENT] Process for #{name} is dead now."
       end
@@ -76,15 +77,15 @@ module Streambox
     end
 
     def stop!
-      logger.debug "[RESILIENT] ================================================== STOP!"
+      logger.debug "[RESILIENT] stop!: ================================================== STOP!"
       if watchdog and watchdog.alive? and @pid
-        logger.debug "[RESILIENT] Killing watchdog..."
+        logger.debug "[RESILIENT] stop!: Killing watchdog..."
         watchdog.kill
-        logger.debug "[RESILIENT] Killing process #{@pid}..."
+        logger.debug "[RESILIENT] stop!: Killing process #{@pid}..."
         kill!
-        logger.debug "[RESILIENT] Killed it."
+        logger.debug "[RESILIENT] stop!: Killed it."
       else
-        logger.debug "[RESILIENT] Stop #{name} but no watchdog. Attempt to kill all..."
+        logger.debug "[RESILIENT] stop!: Stop #{name} but no watchdog. Attempt to kill all..."
         kill_all!
       end
     end
@@ -97,40 +98,41 @@ module Streambox
 
     def kill!
       if @pid.nil?
-        logger.debug "[RESILIENT] Kill what? The pid is nil!"
+        logger.debug "[RESILIENT] kill!: Kill what? The pid is nil!"
         return
       end
-      logger.debug "[RESILIENT] Killing pid #{@pid} and REMOVING PIDFILE!"
+      logger.debug "[RESILIENT] kill!: Killing pid #{@pid} and REMOVING PIDFILE!"
       File.unlink(@pidfile)
       _pid = @pid
       @pid = nil
       cmd = "kill -HUP -#{_pid}"
-      logger.debug "[RESILIENT] Exec: #{cmd}"
+      logger.debug "[RESILIENT] kill!: exec: #{cmd}"
       system(cmd)
     end
 
-    def start_new(delay=0)
-      sleep delay
-      logger.debug "[RESILIENT] spawn: #{cmd}"
+    def start_new
+      logger.debug "[RESILIENT] start_new: spawn: #{cmd}"
       @pid = Process.spawn(cmd, pgroup: true)
       File.open(@pidfile, 'w') { |f| f.print(@pid) }
-      logger.debug "[RESILIENT] New pid for #{name} #{@pid}"
+      logger.debug "[RESILIENT] start_new: New pid for #{name} #{@pid}"
     end
 
     def exists?
       return false if @pid.nil?
       path = "/proc/#{@pid}"
       result = File.directory?(path)
-      logger.debug "[RESILIENT] Stale pid #{@pid}?" unless result
+      logger.debug "[RESILIENT] exists?: Stale pid #{@pid}?" unless result
       result
     end
 
     # sometimes dies here with `no implicit conversion from nil to integer`
     def wait
       # this will work if it is a child process
+      logger.debug "[RESILIENT] wait: Waiting for process #{@pid}."
       Process.wait(@pid)
+      logger.debug "[RESILIENT] wait: Process #{@pid} gone."
     rescue Errno::ECHILD
-      logger.debug "[RESILIENT] Not a child watching via procfs..."
+      logger.debug "[RESILIENT] wait: Process #{@pid} is not a child. Observing procfs..."
       # otherwise we'll just check the procfs
       while exists?
         sleep interval
