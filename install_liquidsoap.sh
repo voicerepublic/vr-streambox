8efbbe1ce441ca17951e49d66329d80667754f91
phil <phil@branch14.org>
2017-05-03 16:24:23 +0200

    progress bar

--- install_liquidsoap.sh
+++ install_liquidsoap.sh
@@ -2,16 +2,22 @@
 
 sudo apt-get install -y --force-yes \
      opam m4 libtag1-dev libogg-dev libvorbis-dev \
-     libmad0-dev libmp3lame-dev libpcre3-dev libasound2-dev
-
-opam init --yes
+     libmad0-dev libmp3lame-dev libpcre3-dev libasound2-dev pv
 
 # speed up by placing precompiled opam folder
 ( cd ..
+  echo
+  echo "Fetching pre built opam..."
+  echo
   wget http://voicerepublic.com/releases/opam.tar.gz
-  tar xfz opam.tar.gz --totals
+  echo "Unpacking opam..."
+  echo
+  echo "Since this will take up to 15 minutes, we'll show you a bogus progress bar."
+  echo
+  pv opam.tar.gz | tar xz
   rm opam.tar.gz )
 
+opam init --yes
 opam install --yes taglib mad lame vorbis cry alsa liquidsoap
 
 # see https://github.com/savonet/liquidsoap-daemon
