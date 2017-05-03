485f3e1aac16b99b9f8376f36727e064081062ff
phil <phil@branch14.org>
2017-05-03 11:37:02 +0200

    force yes & use correct path

--- install_liquidsoap.sh
+++ install_liquidsoap.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install -y \
+sudo apt-get install -y --force-yes \
      opam m4 libtag1-dev libogg-dev libvorbis-dev \
      libmad0-dev libmp3lame-dev libpcre3-dev libasound2-dev
 
 opam init --yes
 
-pwd
-
 # speed up by placing precompiled opam folder
-wget http://voicerepublic.com/releases/opam.tar.gz
-tar xfvz opam.tar.gz
-rm opam.tar.gz
+( cd ..
+  wget http://voicerepublic.com/releases/opam.tar.gz
+  tar xfvz opam.tar.gz
+  rm opam.tar.gz )
 
 opam install --yes taglib mad lame vorbis cry alsa liquidsoap
 
